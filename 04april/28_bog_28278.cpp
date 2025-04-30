913e85f5766c7f6a30a0ad24ff73b10681370efe
SDE00 <ongdongeun00@gmail.com>
2025-04-30 10:37:49 +0900

    1. 월별 폴더명 규칙 변경  2. 5월 기본 구조 추가

---
+++ 04april/28_bog_28278.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, trash; 
+    int x[1000000];
+    int top = 0;
+    
+    cin >> n;
+
+    while (n >0)
+    { 
+        int command;
+        cin >> command;
+
+        if (command == 1)
+        {
+            if (top >= 1000000) {
+                cin >> trash;
+            }
+            else {
+                cin >> x[top];
+                top++;
+            }
+        }
+        else if (command == 2)
+        {
+            if (top <= 0)
+            {
+                cout << -1 << "\n";
+            } else
+            {
+                cout << x[top - 1] << "\n";
+                top--;
+            }
+        }
+        else if (command == 3) // 전체 개수
+        {
+            cout << top << "\n";
+        }
+        else if (command == 4) // 스택이 비어있는지?
+        {
+            if(top <= 0)
+                cout << 1 << "\n";
+            else
+                cout << 0 << "\n";
+        
+        }
+        else if (command == 5)
+        {
+            if( top <= 0)
+                cout << -1 << "\n";
+            else
+                cout << x[top - 1] << "\n";
+        }
+
+        n--;
+    }
+    
+
+
+    return 0;
+}
---
+++ 04april/29_bog_18258.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+    string command;
+
+    while (n--) {
+        cin >> command;
+
+        if (command == "push") {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if (command == "pop") {
+            if (q.empty()) cout << -1 << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (command == "size") {
+            cout << q.size() << '\n';
+        }
+        else if (command == "empty") {
+            cout << (q.empty() ? 1 : 0) << '\n';
+        }
+        else if (command == "front") {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        }
+        else if (command == "back") {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+ 
+    return 0;
+}
