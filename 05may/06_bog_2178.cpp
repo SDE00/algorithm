7058b5340ac8b232d43104680d183a52342a926e
SDE00 <songdongeun00@gmail.com>
2025-07-10 16:04:49 +0900

    알고리즘 풀이

--- 05may/06_bog_2178.cpp
+++ 05may/06_bog_2178.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
-#include <queue>
 #include <string>
+#include <queue>
 using namespace std;
-
-int n, m;
-int a[100][100];      // 미로 지도
-int dist[100][100];   // 최소 칸 수 저장
-
-// 상하좌우 이동 방향 (dx[i], dy[i])
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
+    int n, m;
     cin >> n >> m;
+    int a[100][100];
+    int dist[100][100];
 
-    for (int i = 0; i < n; i++) {
+    //방향 백터: 상 하 좌 우
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+
+    for(int i = 0; i < n; i++) {
         string row;
         cin >> row;
-        for (int j = 0; j < m; j++) {
-            a[i][j] = row[j] - '0';  // char → int
+        for(int j=0; j<m; j++){
+            a[i][j] = row[j] - '0';
         }
     }
 
-    queue<pair<int, int>> q;
+    queue<pair<int, int>> q; // 탐색 큐
     q.push({0, 0});
-    dist[0][0] = 1;  // 시작 지점도 칸에 포함
+    dist[0][0] = 1;
 
     while (!q.empty()) {
         int x = q.front().first;
@@ -48,5 +47,6 @@ int main() {
     }
 
     cout << dist[n-1][m-1] << '\n';
+
     return 0;
 }
