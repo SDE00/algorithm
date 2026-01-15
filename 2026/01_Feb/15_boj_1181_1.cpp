36eef129f034e9750ebc20140f7c3c89f520d77a
송동은 <151814440+SDE00@users.noreply.github.com>
2026-01-15 12:00:04 +0900

    백준 1181 문자열 정렬 및 중복제거

---
+++ 2026/01_Feb/15_boj_1181_1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+bool compare(const string& a, const string& b){
+    if(a.length() != b.length())
+        return a.length() < b.length();
+    return a < b;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    string word[n];
+
+    //입력
+    for(int i=0; i<n; i++){
+        cin >> word[i];
+    }
+
+    //정렬 (길이 짧은 순 / 사전 순)
+    sort(word, word+n, compare);
+
+    //중복 제거
+    int new_end = unique(word, word+n) - word;
+
+    //출력
+    for(int i=0; i<new_end; i++){
+        cout << word[i] << "\n";
+    }
+
+    return 0;
+}
